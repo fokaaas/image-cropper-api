1b2715fb22c577e47f97a1430c730826caa909cc
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-11-17 18:47:02 +0200

    Added author

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import * as process from 'process';
+
+const port = process.env.PORT ?? 3000;
 
 async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '/static/'));
 
-  await app.listen(3000, () => console.info('Server started on 127.0.0.1:3000'));
+  await app.listen(port, () => console.info(`Server started on 127.0.0.1:${port}`));
 }
 
 bootstrap();
